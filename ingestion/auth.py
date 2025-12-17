2b8d687d7593738dfaf5b098548167808f551dd4
tech9-HQ <devops@tech9labs.com>
2025-12-17 12:03:21 +0530

    V02

--- ingestion/auth.py
+++ ingestion/auth.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TENANT_ID = os.getenv("TENANT_ID")
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+TENANT_ID = os.getenv("AZURE_TENANT_ID")
+CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
+CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
 
 TOKEN_URL = f"https://login.microsoftonline.com/{TENANT_ID}/oauth2/v2.0/token"
 SCOPE = "https://graph.microsoft.com/.default"
